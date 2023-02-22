998abb2dd2691a59fe6bd19a5e504f8129c57be1
Christianfitz22 <christianfitz456@gmail.com>
2023-02-21 21:59:04 -0500

    Finished AI for dice game

--- Assets/Scripts/DiceController.cs
+++ Assets/Scripts/DiceController.cs
@@ -1,22 +1,226 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class DiceController : MonoBehaviour
 {
-    public GameObject die1;
-    public GameObject die2;
-    public GameObject die3;
+    public Die die1;
+    public Die die2;
+    public Die die3;
+
+    public Vector3 playerPoint;
+    public Vector3 enemyPoint;
+
+    private float elapsedTime = 0f;
+    private bool waitingForRoll = false;
+
+    private bool playerTurn = true;
+
+    // game stats
+
+    private int playerChips;
+    private int enemyChips;
+    private int pot;
+    private int targetNumber;
+
+    public TMP_Text display;
+    public GameObject turnIndicator;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerChips = 5;
+        enemyChips = 5;
+        pot = 0;
+        targetNumber = 0;
+        displayScore();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (waitingForRoll)
+        {
+            elapsedTime -= Time.deltaTime;
+            if (elapsedTime < 0f)
+            {
+                int result = getSum();
+                if (result == -1)
+                {
+                    Debug.Log("bad throw");
+                    beginRoll();
+                }
+                else
+                {
+                    Debug.Log(result);
+                    waitingForRoll = false;
+                    if (result >= targetNumber)
+                    {
+                        targetNumber = result;
+                    }
+                    else
+                    {
+                        targetNumber = 0;
+                        if (playerTurn)
+                        {
+                            enemyChips += pot;
+                        }
+                        else
+                        {
+                            playerChips += pot;
+                        }
+                        pot = 0;
+                    }
+                    playerTurn = !playerTurn;
+                    displayScore();
+                }
+            }
+        }
+        else
+        {
+            // enemy AI
+            if (!playerTurn)
+            {
+                if (playerChips == 0 && pot == 0)
+                {
+                    // run player loss dialogue
+                    Debug.Log("player loss");
+                    foldMove();
+                }
+                else if (enemyChips == 0)
+                {
+                    foldMove();
+                    // run enemy loss dialogue
+                    Debug.Log("enemy loss");
+                }
+                else if ((targetNumber > 13 && pot < 2))
+                {
+                    foldMove();
+                }
+                else
+                {
+                    raiseMove();
+                }
+            }
+
+            // player options
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                raiseMove();
+            }
+            if (Input.GetKeyDown(KeyCode.C))
+            {
+                foldMove();
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            playerChips = 5;
+            enemyChips = 5;
+            pot = 0;
+            targetNumber = 0;
+            displayScore();
+            playerTurn = true;
+            waitingForRoll = false;
+            elapsedTime = 0f;
+        }
+    }
+
+    public void raiseMove()
+    {
+        if (playerTurn)
+        {
+            if (playerChips > 0)
+            {
+                pot += 1;
+                playerChips -= 1;
+                displayScore();
+                beginRoll();
+            }
+        }
+        else
+        {
+            if (enemyChips > 0)
+            {
+                pot += 1;
+                enemyChips -= 1;
+                displayScore();
+                beginRoll();
+            }
+        }
+    }
+
+    public void foldMove()
+    {
+        if (playerTurn)
+        {
+            enemyChips += pot;
+        }
+        else
+        {
+            playerChips += pot;
+        }
+        pot = 0;
+        targetNumber = 0;
+        playerTurn = !playerTurn;
+        displayScore();
+    }
+
+    public void displayScore()
+    {
+        string score = "Your Chips: " + playerChips + "     Their Chips: " + enemyChips + 
+            "\nChips in Pot: " + pot + "     Roll To Beat: " + targetNumber;
+        display.SetText(score);
+        Vector3 baseScale = turnIndicator.transform.localScale;
+        if (playerTurn)
+        {
+            turnIndicator.transform.localScale = new Vector3(baseScale.x, baseScale.y, Mathf.Abs(baseScale.z));
+        }
+        else
+        {
+            turnIndicator.transform.localScale = new Vector3(baseScale.x, baseScale.y, -Mathf.Abs(baseScale.z));
+        }
+    }
+
+    public void beginRoll()
+    {
+        if (playerTurn)
+        {
+            rollAll(playerPoint, 1);
+        }
+        else
+        {
+            rollAll(enemyPoint, -1);
+        }
+        waitingForRoll = true;
+        elapsedTime = 1.5f;
+    }
+
+    public void rollAll(Vector3 position, int direction)
+    {
+        die1.roll(position, direction);
+        die2.roll(position, direction);
+        die3.roll(position, direction);
+    }
+
+    public void rollAll()
+    {
+        die1.roll();
+        die2.roll();
+        die3.roll();
+    }
+
+    public int getSum()
+    {
+        int result1 = die1.getResult();
+        int result2 = die2.getResult();
+        int result3 = die3.getResult();
+        if (result1 == -1 || result2 == -1 || result3 == -1)
+        {
+            return -1;
+        }
+        return result1 + result2 + result3;
     }
 }
--- Assets/Scripts/Die.cs
+++ Assets/Scripts/Die.cs
@@ -22,7 +22,27 @@ public class Die : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            //roll();
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            //Debug.Log(getResult());
+        }
+    }
 
+    public void roll(Vector3 position, int direction)
+    {
+        transform.position = position + throwPos;
+        transform.rotation = Random.rotation;
+        rb.velocity = Vector3.zero;
+        float directionDegrees = Random.Range(-throwRadius, throwRadius);
+        float directionRadians = directionDegrees * Mathf.PI / 180f;
+        float forceZ = throwForce * Mathf.Cos(directionRadians);
+        float forceX = throwForce * Mathf.Sin(directionRadians);
+        Vector3 force = new Vector3(forceX, 0f, direction * forceZ);
+        rb.AddForce(force, ForceMode.VelocityChange);
     }
 
     public void roll()
@@ -39,47 +59,53 @@ public class Die : MonoBehaviour
     }
 
     /*
-     1 = 0 Y 0
-     2 = 0 Y -90
-     3 = 90 0 Z
-     4 = -90 0 Z
-     5 =  0 Y 90
-     6 = -180 Y 0
+     1 = 0 y 0
+     2 = 0 y 270
+     3 = 90 y 0
+     4 = 270 y 0
+     5 = 0 y 90
+     6 = 0 y 180
      */
 
     public int getResult()
     {
-        Vector3 rotation = transform.localRotation.eulerAngles;
-        if (Mathf.Aprroximately(rotation.x, 0f))
+        Vector3 rotation = transform.rotation.eulerAngles;
+        Debug.Log(transform.rotation.eulerAngles);
+        if (closeEnough(rotation.x, 0f) || closeEnough(rotation.x, 360f))
         {
-            if (Mathf.Approximately(rotation.z, 0f))
+            if (closeEnough(rotation.z, 0f) || closeEnough(rotation.z, 360f))
             {
                 return 1;
             }
-            if (Mathf.Approximately(rotation.z, -90f))
+            if (closeEnough(rotation.z, 270f))
             {
                 return 2;
             }
-            if (Mathf.Approximately(rotation.z, 90f))
+            if (closeEnough(rotation.z, 90f))
             {
                 return 5;
             }
+            if (closeEnough(rotation.z, 180f))
+            {
+                return 6;
+            }
         }
-        if (Mathf.Approximately(rotation.y, 0f))
+        if (closeEnough(rotation.z, 0f) || closeEnough(rotation.z, 360f))
         {
-            if (Mathf.Approximately(rotation.x, 90f))
+            if (closeEnough(rotation.x, 90f))
             {
                 return 3;
             }
-            if (Mathf.Approximately(rotation.x, -90f))
+            if (closeEnough(rotation.x, 270f))
             {
                 return 4;
             }
         }
-        if (Mathf.Approximately(rotation.z, 0f) && Mathf.Approximately(rotation.x, -180f))
-        {
-            return 6;
-        }
         return -1;
+    }
+
+    public bool closeEnough(float f1, float f2)
+    {
+        return Mathf.Abs(f1 - f2) < 5f;
     }
 }
